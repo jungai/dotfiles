076a5c680475e3709d4fea7ceeb3a3af45bacad2
jungai <juworaphol125@gmail.com>
2021-05-31 14:06:16 +0700

    forgot fish

---
+++ fish/config.fish
@@ -0,0 +1,29 @@
+set -gx EDITOR vim
+
+# Homebrew package manager
+if test -x ~/.linuxbrew/bin
+    eval (~/.linuxbrew/bin/brew shellenv)
+end
+
+# asdf
+source ~/.asdf/asdf.fish
+
+# asdf should exec first
+export PATH="$PATH:`yarn global bin`"
+
+# alias
+alias c="clear"
+alias d="yarn dev"
+alias s="yarn start"
+alias tm="tmux"
+alias yc="yarn compile"
+alias tmk='tmux kill-server'
+alias pc="pwd | pbcopy"
+alias p="pnpm"
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
+
+if test (which exa)
+	alias ls="exa -l -a --icons -h"
+end
+
