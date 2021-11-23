7f50a10f69751c7751fa71f90c988921de8ab080
jungai <juworaphol125@gmail.com>
2021-11-23 23:57:56 +0700

    add fish for wsl2

---
+++ fish/wsl.fish
@@ -0,0 +1,15 @@
+set -gx EDITOR vim
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+source ~/.asdf/asdf.fish
+
+# Homebrew package manager
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+if test (which exa)
+	alias ls="exa -l -a -h"
+end
