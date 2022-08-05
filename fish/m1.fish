a632bc1ac66ccff1c3dc8a5a6f8bc202544e8233
jungai <juworaphol125@gmail.com>
2022-08-05 14:55:43 +0700

    [fish] alias start:dev

--- fish/m1.fish
+++ fish/m1.fish
@@ -36,6 +36,7 @@ alias gone='git for-each-ref --format \'%(refname:short) %(upstream:track)\' | \
   awk \'$2 == "[gone]" {print $1}\' | \
   xargs -r git branch -D'
 alias ss='pnpm start'
+alias sd='pnpm start:dev'
 alias dd='pnpm dev'
 alias bb='pnpm build'
 
