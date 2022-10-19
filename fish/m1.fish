d925266fc05d59b640eef928cce83977559b472f
jungai <juworaphol125@gmail.com>
2022-10-19 17:05:38 +0700

    [fish] add Catppuccin theme for fzf

--- fish/m1.fish
+++ fish/m1.fish
@@ -90,5 +90,11 @@ atuin init fish | source
 set -Ux BUN_INSTALL "/Users/jungai/.bun"
 set -px --path PATH "/Users/jungai/.bun/bin"
 
+# Catppuccin
+set -Ux FZF_DEFAULT_OPTS "\
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
 # Java
 . ~/.asdf/plugins/java/set-java-home.fish
