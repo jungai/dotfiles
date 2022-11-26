74f58e1c593ea100a5d4878b4afb4aeb36bbf382
jungai <juworaphol125@gmail.com>
2022-11-26 18:26:20 +0700

    [nvim] update plugin

--- nvim/lua/jungai/plugins-setup.lua
+++ nvim/lua/jungai/plugins-setup.lua
@@ -1,20 +1,22 @@
+-- auto install packer if not installed
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
     return true
   end
   return false
 end
-
 local packer_bootstrap = ensure_packer()
 
-vim.cmd([[
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
   augroup end
 ]])
 
@@ -26,8 +28,15 @@ end
 
 return packer.startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'nvim-tree/nvim-web-devicons'
+  use 'nvim-lua/plenary.nvim'
   use 'bluz71/vim-nightfly-colors'
-
+  use 'szw/vim-maximizer'
+  use 'tpope/vim-surround'
+  use 'numToStr/Comment.nvim'
+  use 'nvim-tree/nvim-tree.lua'
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
   if packer_bootstrap then
     require('packer').sync()
