af0d957ecb4180693a445441ead44ba69b376fa8
jungai <juworaphol125@gmail.com>
2022-11-26 18:26:57 +0700

    [nvim] update keymap

--- nvim/lua/jungai/core/keymaps.lua
+++ nvim/lua/jungai/core/keymaps.lua
@@ -9,3 +9,26 @@ keymap.set("n", "x", "\"_x")
 keymap.set("n", "<leader>+", "<C-a>")
 keymap.set("n", "<leader>-", "<C-x>")
 
+-- window
+keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sh", "<C-w>s")
+keymap.set("n", "<leader>se", "<C-w>=")
+keymap.set("n", "<leader>sw", ":close<CR>")
+
+-- tab
+keymap.set("n", "<leader>to", ":tabnew<CR>")
+keymap.set("n", "<leader>tw", ":tabclose<CR>")
+keymap.set("n", "<leader>[", ":tabn<CR>")
+keymap.set("n", "<leader>]", ":tabp<CR>")
+
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+
+-- tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
