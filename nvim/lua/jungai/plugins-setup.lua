e0c868118f48d44f03d26a76438f611ad0350f85
jungai <juworaphol125@gmail.com>
2022-11-26 20:16:01 +0700

    [nvim] update plugins

--- nvim/lua/jungai/plugins-setup.lua
+++ nvim/lua/jungai/plugins-setup.lua
@@ -1,13 +1,13 @@
 -- auto install packer if not installed
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 local packer_bootstrap = ensure_packer()
 
@@ -23,22 +23,47 @@ vim.cmd([[
 local ok, packer = pcall(require, "packer")
 
 if not ok then
-  return
+	return
 end
 
 return packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'nvim-tree/nvim-web-devicons'
-  use 'nvim-lua/plenary.nvim'
-  use 'bluz71/vim-nightfly-colors'
-  use 'szw/vim-maximizer'
-  use 'tpope/vim-surround'
-  use 'numToStr/Comment.nvim'
-  use 'nvim-tree/nvim-tree.lua'
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+	use("wbthomason/packer.nvim")
+	use("nvim-tree/nvim-web-devicons")
+	use("nvim-lua/plenary.nvim")
+	use("bluz71/vim-nightfly-colors")
+	use("szw/vim-maximizer")
+	use("tpope/vim-surround")
+	use("numToStr/Comment.nvim")
+	use("nvim-tree/nvim-tree.lua")
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
+	use("onsails/lspkind.nvim")
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use({ "glepnir/lspsaga.nvim", branch = "main" })
+	use("jose-elias-alvarez/typescript.nvim")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jayp0521/mason-null-ls.nvim")
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
+	-- auto closing
+	use("windwp/nvim-autopairs")
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" })
 
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
