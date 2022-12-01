89afa16a718a502f97609a49ac7ddca1d60af9f7
jungai <juworaphol125@gmail.com>
2022-12-01 23:00:41 +0700

    [nvim] telescope show hidden file

--- nvim/lua/jungai/plugins/telescope.lua
+++ nvim/lua/jungai/plugins/telescope.lua
@@ -1,24 +1,35 @@
 local telescope_setup, telescope = pcall(require, "telescope")
 if not telescope_setup then
-  return
+	return
 end
 
 local actions_setup, actions = pcall(require, "telescope.actions")
 if not actions_setup then
-  return
+	return
 end
 
 telescope.setup({
-  -- configure custom mappings
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-        ["<C-j>"] = actions.move_selection_next, -- move to next result
-        -- ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
-      },
-    },
-  },
+	-- configure custom mappings
+	defaults = {
+		mappings = {
+			i = {
+				["<C-k>"] = actions.move_selection_previous, -- move to prev result
+				["<C-j>"] = actions.move_selection_next, -- move to next result
+				-- ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+			},
+		},
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+		live_grep = {
+			hidden = true,
+		},
+		file_browser = {
+			hidden = true,
+		},
+	},
 })
 
 telescope.load_extension("fzf")
