b4be1352139456d1c8a51ee29e39408689fbc7f1
jungai <juworaphol125@gmail.com>
2022-12-02 21:21:35 +0700

    [nvim] update keymap

--- nvim/lua/jungai/core/keymaps.lua
+++ nvim/lua/jungai/core/keymaps.lua
@@ -3,7 +3,7 @@ local keymap = vim.keymap
 
 keymap.set("n", "<leader>nh", ":noh<CR>")
 -- not save to buffer
-keymap.set("n", "x", "\"_x")
+keymap.set("n", "x", '"_x')
 
 -- adding number
 keymap.set("n", "<leader>+", "<C-a>")
@@ -24,7 +24,8 @@ keymap.set("n", "<leader>]", ":tabp<CR>")
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 
 -- tree
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+-- keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<CR>")
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
@@ -32,3 +33,4 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+keymap.set("n", "<leader>fe", "<cmd>Telescope file_browser<cr>")
