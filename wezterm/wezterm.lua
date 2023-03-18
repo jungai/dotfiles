66007e3343085c68f38380f93aef7b6627efdc64
jungai <juworaphol125@gmail.com>
2023-03-18 11:14:55 +0700

    [wezterm] add font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 
 return {
 	font = wezterm.font("Menlo"),
+	font_size = 14,
 	color_scheme = "VSCodeDark+ (Gogh)",
 	window_background_opacity = 0.9,
 	window_decorations = "RESIZE",
