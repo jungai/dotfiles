0ab42c2d5a409f59514902397ced655dfaa32144
jungai <juworaphol125@gmail.com>
2023-03-20 09:35:50 +0700

    [wezterm] remove padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,4 +6,10 @@ return {
 	color_scheme = "VSCodeDark+ (Gogh)",
 	window_background_opacity = 0.9,
 	window_decorations = "RESIZE",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
 }
