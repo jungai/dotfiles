171b89d96ca793463c4319e2bb8412a36d686322
jungai <juworaphol125@gmail.com>
2023-08-28 14:11:22 +0700

    [wezterm] lofi kub

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,6 +6,7 @@ return {
 	color_scheme = "Rosé Pine (base16)",
 	window_background_opacity = 0.95,
 	window_decorations = "RESIZE",
+
 	window_padding = {
 		left = 0,
 		right = 0,
@@ -13,4 +14,14 @@ return {
 		bottom = 0,
 	},
 	hide_tab_bar_if_only_one_tab = true,
+	window_background_image = "/Users/jungai/Desktop/lofi.jpg",
+	window_background_image_hsb = {
+		-- Darken the background image by reducing it to 1/3rd
+		brightness = 0.01,
+		-- You can adjust the hue by scaling its value.
+		-- a multiplier of 1.0 leaves the value unchanged.
+		hue = 1.0,
+		-- You can adjust the saturation also.
+		saturation = 1.0,
+	},
 }
