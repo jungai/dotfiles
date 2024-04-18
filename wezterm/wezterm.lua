37dd2fb1bc592f0eed36a3a40abb134b9a9c4ebd
jungai <juworaphol125@gmail.com>
2024-04-18 19:17:32 +0700

    [wezterm] DankMono Nerd Font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,18 +8,18 @@ return {
 		-- "hack Nerd Font",
 		-- "Zed Mono Extended",
 		-- "Monaspace Neon",
+		-- "JetBrains Mono",
 		{
-			family = "Dank Mono",
+			family = "DankMono Nerd Font",
 			italic = false,
 		},
-		-- "JetBrains Mono",
 	}),
 	front_end = "OpenGL",
 	font_size = 18,
 	line_height = 1.618,
 	color_scheme = "Catppuccin Macchiato",
 	window_background_opacity = 0.90,
-	window_decorations = "RESIZE",
+	window_decorations = "TITLE",
 	window_padding = {
 		left = 0,
 		right = 0,
@@ -27,14 +27,4 @@ return {
 		bottom = 0,
 	},
 	hide_tab_bar_if_only_one_tab = true,
-	-- window_background_image = "/Users/worapholw/Desktop/lofi.jpg",
-	-- window_background_image_hsb = {
-	-- 	-- Darken the background image by reducing it to 1/3rd
-	-- 	brightness = 0.01,
-	-- 	-- You can adjust the hue by scaling its value.
-	-- 	-- a multiplier of 1.0 leaves the value unchanged.
-	-- 	hue = 1.0,
-	-- 	-- You can adjust the saturation also.
-	-- 	saturation = 1.0,
-	-- },
 }
