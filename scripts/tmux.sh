65afb9001e4847f0d5ee46bb48bcd7f63651b759
jungai <juworaphol125@gmail.com>
2024-12-30 00:08:48 +0700

    [script] should check is in session or not

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -2,10 +2,15 @@
 
 session="$(tmux ls | fzf --reverse)"
 
-if [[ "$session" ]]; then
-	target="${session:0:1}"
-	tmux attach-session -t $target
+if [[ "$TERM_PROGRAM" = "tmux" ]]; then
+	echo "Already in a tmux session"
+
 else
-	echo "No session selected"
-	return 0
+	if [[ "$session" ]]; then
+		target="${session:0:1}"
+		tmux attach-session -t $target
+	else
+		echo "No session selected"
+		return 0
+	fi
 fi
