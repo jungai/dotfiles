aabc5c3a5cfb7a2c6826175f6fb7f2839805cb05
jungai <juworaphol125@gmail.com>
2025-03-01 15:05:36 +0700

    [nvim] clipboard

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -114,9 +114,7 @@ vim.opt.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
+vim.opt.clipboard:append 'unnamedplus'
 
 -- Enable break indent
 vim.opt.breakindent = true
