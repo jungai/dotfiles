0738778d4c1e4b83faac8ee06d0285f5627e824d
jungai <juworaphol125@gmail.com>
2025-04-24 10:32:34 +0700

    [nvim] show current line blame

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -279,6 +279,7 @@ require('lazy').setup({
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
+      current_line_blame = true,
       signs = {
         add = { text = '+' },
         change = { text = '~' },
