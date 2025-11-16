36d8bb0ee40ee8734f39df2b62de4edaa5c5eace
jungai <juworaphol125@gmail.com>
2025-11-16 22:49:46 +0700

    [nvim] ++

--- .config/nvim/lua/kickstart/plugins/tiny-inline-diagnostic.lua
+++ .config/nvim/lua/kickstart/plugins/tiny-inline-diagnostic.lua
@@ -3,11 +3,14 @@ return {
   event = 'VeryLazy',
   priority = 1000,
   opts = {
+    preset = 'powerline',
     options = {
       add_messages = {
         display_count = true,
+        messages = true,
       },
       multilines = {
+        always_show = true,
         enabled = true,
       },
     },
---
+++ .config/nvim/lua/kickstart/plugins/ts-error-translator.lua
@@ -0,0 +1,5 @@
+return {
+  'dmmulroy/ts-error-translator.nvim',
+  event = 'VeryLazy',
+  opts = {},
+}
