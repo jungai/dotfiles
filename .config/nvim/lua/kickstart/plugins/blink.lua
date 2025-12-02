71394e3a4a81378678880528685c6822fb50b606
jungai <juworaphol125@gmail.com>
2025-12-02 09:30:58 +0700

    [nvim] ++

--- .config/nvim/lua/kickstart/plugins/blink.lua
+++ .config/nvim/lua/kickstart/plugins/blink.lua
@@ -37,48 +37,6 @@ return {
     completion = {
       menu = {
         border = 'single',
-        draw = {
-          components = {
-            kind_icon = {
-              text = function(ctx)
-                if vim.tbl_contains({ 'Path' }, ctx.source_name) then
-                  local mini_icon, _ = require('mini.icons').get_icon(ctx.item.data.type, ctx.label)
-                  if mini_icon then
-                    return mini_icon .. ctx.icon_gap
-                  end
-                end
-
-                local icon = require('lspkind').symbolic(ctx.kind, { mode = 'symbol' })
-                return icon .. ctx.icon_gap
-              end,
-
-              -- Optionally, use the highlight groups from mini.icons
-              -- You can also add the same function for `kind.highlight` if you want to
-              -- keep the highlight groups in sync with the icons.
-              highlight = function(ctx)
-                if vim.tbl_contains({ 'Path' }, ctx.source_name) then
-                  local mini_icon, mini_hl = require('mini.icons').get_icon(ctx.item.data.type, ctx.label)
-                  if mini_icon then
-                    return mini_hl
-                  end
-                end
-                return ctx.kind_hl
-              end,
-            },
-            kind = {
-              -- Optional, use highlights from mini.icons
-              highlight = function(ctx)
-                if vim.tbl_contains({ 'Path' }, ctx.source_name) then
-                  local mini_icon, mini_hl = require('mini.icons').get_icon(ctx.item.data.type, ctx.label)
-                  if mini_icon then
-                    return mini_hl
-                  end
-                end
-                return ctx.kind_hl
-              end,
-            },
-          },
-        },
       },
       documentation = { auto_show = true, window = { border = 'single' } },
     },
