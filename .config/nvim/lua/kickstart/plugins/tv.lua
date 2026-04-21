ccd6f831aff15b6a9663bbfcd93020e0b845d648
jungai <juworaphol125@gmail.com>
2026-04-21 09:45:35 +0700

    [nvim] update

--- .config/nvim/lua/kickstart/plugins/tv.lua
+++ .config/nvim/lua/kickstart/plugins/tv.lua
@@ -19,7 +19,7 @@ return {
           -- what happens when you press a key
           handlers = {
             ['<CR>'] = h.open_as_files, -- default: open selected files
-            ['<C-q>'] = h.send_to_quickfix, -- send to quickfix list
+            ['<C-l>'] = h.send_to_quickfix, -- send to quickfix list
             ['<C-s>'] = h.open_in_split, -- open in horizontal split
             ['<C-v>'] = h.open_in_vsplit, -- open in vertical split
             ['<C-y>'] = h.copy_to_clipboard, -- copy paths to clipboard
@@ -30,7 +30,7 @@ return {
           keybinding = '<leader>fg',
           handlers = {
             ['<CR>'] = h.open_at_line, -- Jump to line:col in file
-            ['<C-q>'] = h.send_to_quickfix, -- Send matches to quickfix
+            ['<C-l>'] = h.send_to_quickfix, -- Send matches to quickfix
             ['<C-s>'] = h.open_in_split, -- Open in horizontal split
             ['<C-v>'] = h.open_in_vsplit, -- Open in vertical split
             ['<C-y>'] = h.copy_to_clipboard, -- Copy matches to clipboard
